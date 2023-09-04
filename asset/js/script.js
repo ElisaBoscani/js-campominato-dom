e07abb9b197b83d683db21c5aa48be2c1cc36ba5
Elisa Boscani <elisabosc90@gmail.com>
2023-09-04 23:24:00 +0200

    select difficulty

--- asset/js/script.js
+++ asset/js/script.js
@@ -13,64 +13,124 @@ cioè il numero di volte che l’utente ha cliccato su una cella che non era una
 
 //elementi del DOM
 const startGame = document.querySelector("button");
+const scoreBox = document.getElementById("scoreBox");
+const score = document.getElementById("score");
+/* const difficulty = document.getElementById("difficulty").value; */
 const arrayBombs = [];
+const arrayButtons = document.getElementsByClassName("square");
 const gridEl = document.getElementById("grid");
 gridEl.classList.add("dNone");
-let clearGrid = false;
+
+const select = document.getElementById("difficulty");
+let total = 0;
+let squareNumber = 100;
+select.addEventListener("change", function (e) {
+  selecLevel(e);
+});
+
 //L'utente clicca su un bottone che genererà una griglia di gioco
 startGame.addEventListener("click", function () {
-  if (!clearGrid) {
-    genereteElement(gridEl, squareNumber);
-    clearGrid = true;
-  }
-  if (gridEl.classList.contains("dNone")) {
+  gridEl.innerHTML = "";
+
+  //creiamo le difficolta
+  genereteElement(gridEl, squareNumber);
+
+  //l'apparire e scomparire della griglia
+  if (
+    gridEl.classList.contains("dNone") ||
+    scoreBox.classList.contains("dNone")
+  ) {
     gridEl.classList.remove("dNone");
     gridEl.classList.add("d-flex");
+    scoreBox.classList.remove("dNone");
   } else {
     gridEl.classList.add("dNone");
     gridEl.classList.remove("d-flex");
+    scoreBox.classList.add("dNone");
   }
+
   /* creiamo le bombe */
   while (arrayBombs.length < 16) {
     const numberBomb = Math.floor(Math.random() * 100) + 1;
-    console.log(numberBomb);
+
     if (!arrayBombs.includes(numberBomb)) {
       arrayBombs.push(numberBomb);
     }
   }
+
+  for (let i = 0; i < arrayButtons.length; i++) {
+    const button = arrayButtons[i];
+    button.disabled = false;
+  }
   console.log(arrayBombs);
 });
 
+//livelli di difficoltà
+function selecLevel(e) {
+  if (e.target.value === "easy") {
+    squareNumber = 100;
+  } else if (e.target.value === "medium") {
+    squareNumber = 81;
+  } else if (e.target.value === "hard") {
+    squareNumber = 49;
+  }
+}
+
 //funzione per creare elementi
 
-const squareNumber = 100;
 function genereteElement(element, number) {
   //creato il quadratini e il testo(che parte con un display non, perche si vedra solo al click del quadrato)
+  console.log("number", number);
+  console.log("element", element);
   for (let i = 0; i < number; i++) {
-    const square = document.createElement("div");
+    const square = document.createElement("button");
     const squareText = document.createElement("p");
 
     squareText.classList.add("dNone");
     square.append(squareText);
     square.classList.add("square");
     element.append(square);
-    console.log(square);
+    if (number === 81) {
+      square.style.width = "calc(100% / 9)";
+    } else if (number === 49) {
+      square.style.width = "calc(100% / 7)";
+    } else {
+      square.style.width = "calc(100% / 10)";
+    }
+
     //cambiare il nome delle celle e il colore
+
     square.addEventListener("click", function () {
       changeColorText(square, squareText);
-
+      //gameover e punteggio
       if (arrayBombs.includes(i + 1)) {
         square.classList.add("bgRed");
         squareText.innerHTML = "💣";
         endGame();
+
+        for (let i = 0; i < arrayButtons.length; i++) {
+          const button = arrayButtons[i];
+          button.disabled = true;
+        }
+
         arrayBombs.splice(0, arrayBombs.length);
+      } else {
+        for (let i = 0; i < arrayButtons.length; i++) {
+          const button = arrayButtons[i];
+          if (button.className === "square bgColor") {
+            button.disabled = true;
+          }
+        }
+        total++;
+        score.innerHTML = total;
+        console.log(total);
       }
     });
+
     startGame.addEventListener("click", function () {
       squareText.innerHTML = "";
       square.classList.remove("bgRed");
       square.classList.add("bgBlue");
-      console.log(square);
     });
   }
 }
